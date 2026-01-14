6dab5aeb4ca9775bac67870b49af363d01a72c21
AetherO-S <aether.os.email@gmail.com>
2026-01-13 21:05:57 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>My Embedded Page</title>
+</head>
+<body>
+<h1>My Game / Content Page</h1>
+<p>Users will see this page inside Google Sites.</p>
+
+<!-- POPADS CODE WILL GO HERE -->
+
+</body>
+</html>
